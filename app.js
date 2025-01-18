84b627ab94cb390ae90c1f56024937effea94587
errorose <yoonyeseul2000@gmail.com>
2025-01-19 01:26:49 +0900

    [study] Function

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
+function sayHello(nameOfPerson, ageOfPerson) {
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + ageOfPerson);
+}
+
+sayHello("kuromi", 10);
+sayHello("pikachu", 20);
+sayHello("kitty", 30);
+
+function plus(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
+}
+
+plus(60, 8);
+
+function divide(firstNumber, secondNumber) {
+    console.log(firstNumber / secondNumber);
+}
+
+divide(98, 20);
+
+console.log("-----");
+
 const player = {
     name: "kuromi",
-    point: 10,
-    animal: true,
+    sayHello: function (otherPersonName) {
+        console.log("Hello! " + otherPersonName);
+    },
 };
 
-console.log(player);
-// console.log(player.name);
-// console.log(player["point"]);
-
-player.animal = false;
-console.log(player.animal);
-
-player.point = player.point + 5;
-console.log(player.point);
-
-player.lastName = "yoon";
-console.log(player);
+console.log(player.name);
+player.sayHello("yeseul");
+player.sayHello("yoon");
