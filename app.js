5b1793e87fae1e45191acb9b82d1e2e40c7178e9
errorose <yoonyeseul2000@gmail.com>
2025-02-28 23:08:27 +0900

    [study] CSS in Javascript

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
-const h1 = document.querySelector(".title h1");
+const h1 = document.querySelector("div.title:first-child h1");
 
-function handleClick() {
-    h1.style.color = "blue";
+function handleTitleClick() {
+    const currentColor = h1.style.color;
+    let newColor;
+
+    if (currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-}
-
-function handleMouseLeave() {
-    h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "skyblue";
-}
-
-function handleWindowCopy() {
-    alert("copier!");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI!");
-}
-
-function handleWindowOnline() {
-    alert("ALL GOOD!");
-}
-
-h1.addEventListener("click", handleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
