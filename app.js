eb3b1cd3a6fa59b031d69c4a2391662fc9db9435
errorose <yoonyeseul2000@gmail.com>
2025-02-28 23:26:16 +0900

    [study] CSS in Javascript part Two

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const h1 = document.querySelector("div.title:first-child h1");
 
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
+    const clickedClass = "active";
 
-    if (currentColor === "blue") {
-        newColor = "tomato";
+    if (h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        newColor = "blue";
+        h1.className = clickedClass;
     }
-
-    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
