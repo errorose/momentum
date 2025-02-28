e6cd280c84b3296dc414fa690f1f55f7a5e4af32
errorose <yoonyeseul2000@gmail.com>
2025-02-28 23:43:33 +0900

    [study] CSS in Javascript part Three

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const h1 = document.querySelector("div.title:first-child h1");
 
 function handleTitleClick() {
-    const clickedClass = "active";
+    // const clickedClass = "active";
 
-    if (h1.className === clickedClass) {
-        h1.className = "";
-    } else {
-        h1.className = clickedClass;
-    }
+    // if (h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass);
+    // } else {
+    //     h1.classList.add(clickedClass);
+    // }
+
+    h1.classList.toggle("active");
 }
 
 h1.addEventListener("click", handleTitleClick);
