8e5c891267189723cfac4256a9d5e689b6f07010
errorose <yoonyeseul2000@gmail.com>
2025-03-04 23:39:57 +0900

    [study] Events

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginBtn = document.querySelector("#login-form button");
 
-function handleLoginBtn() {
+function onLoginSubmit(event) {
+    event.preventDefault();
     console.log(loginInput.value);
 }
 
-loginBtn.addEventListener("click", handleLoginBtn);
+loginForm.addEventListener("submit", onLoginSubmit);
