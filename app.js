d4d67d2ea1f3dff9d62f65832aa88b4b1ef7d457
errorose <yoonyeseul2000@gmail.com>
2025-03-05 23:07:40 +0900

    [study] Saving Username

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const HIDDEN_CLASSNAME = "hidden";
 function onLoginSubmit(event) {
     event.preventDefault();
     const userName = loginInput.value;
+    localStorage.setItem("username", userName); // 로컬스토리지에 값 저장
     loginForm.classList.add(HIDDEN_CLASSNAME);
 
     greeting.innerHTML = `hello ${userName}`;
