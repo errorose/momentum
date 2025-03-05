c514fe46631302fd8b156e50edd92d6c8548cfa6
errorose <yoonyeseul2000@gmail.com>
2025-03-05 23:34:44 +0900

    [study] Loading Username

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const userName = loginInput.value;
-    localStorage.setItem("username", userName); // 로컬스토리지에 값 저장
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    const userName = loginInput.value;
 
-    greeting.innerHTML = `hello ${userName}`;
+    localStorage.setItem(USERNAME_KEY, userName); // 로컬스토리지에 값 저장
+
+    paintGreetings(userName);
+}
+
+function paintGreetings(username) {
+    greeting.innerHTML = `hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+console.log(savedUsername);
+
+if (savedUsername == null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
